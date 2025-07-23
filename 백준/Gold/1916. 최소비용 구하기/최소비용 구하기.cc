2c63591201fd40378c4f48724a54c630fc03f087
ljw2869 <138024372+ljw2869@users.noreply.github.com>
2025-07-23 21:40:46 +0100

    [Gold V] Title: 최소비용 구하기, Time: 24 ms, Memory: 6076 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,66 +1,64 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-
-#define INF 987654321
 using namespace std;
 
-int node[1005][1005]={INF,};
-priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+#define INF 987654321
 
-int Ddist[1005];
+int value[1001];
+int graph[1001][1001];
+
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+//pair중에서 첫번째 값이 작은 순으로 우선순위큐가 만들어진다.
 
 int main(){
+    
     ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);cout.tie(0);
 
     int n,m;
     cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++)
-            node[i][j]=INF;
-    }
-    for(int i=1;i<=n;i++){
-        node[i][i]=0;
-        Ddist[i]=INF;
+
+    for(int i=1;i<=n;i++)
+    {
+        value[i]=INF;
+        for(int j=1;j<=n;j++){
+            graph[i][j]=INF;
+        }
+        graph[i][i]=0;
     }
 
+    int u,v,w;
     for(int i=0;i<m;i++){
-        int u,v,w;
         cin>>u>>v>>w;
-        if(node[u][v]>w){//여기가 핵심!! 두 노드 사이 여러 개의 간선이 연결될 수 있는데 최솟값만을 저장함
-            node[u][v]=w;
+        if(graph[u][v]>w){
+            graph[u][v]=w;
         }
     }
 
-    int start,end;
-    cin>>start>>end;
+    int s,e;
+    cin>>s>>e;
+    value[s]=0;
 
-    Ddist[start]=0;
-
-    pq.push(make_pair(0,start));//각 쌍은 {가중치(거리), 노드}로 저장되어 있음
+    pq.push(make_pair(0,s));
 
     while(!pq.empty()){
-        //최소거리x와 그때의 노드 번호U
-        int x=pq.top().first;
-        int U=pq.top().second;
+        int cost=pq.top().first;//첫번째 값에 가중치가 저장되어 있음
+        int node=pq.top().second;//최소 가중치를 가지는 노드의 번호
         pq.pop();
 
-        //이 노드에서 나가는 간선들을 확인한 후 그 간선에 연결된 노드들의 최소거리를 업데이트
-        for(int i=1;i<=n;i++){ 
-            int W=node[U][i]; //그래프에서의 간선 가중치
-
-            if(W==INF)
-                continue;//볼 가치 없이 건너뛰기
-            
-            if(x+W<Ddist[i]){
-                Ddist[i]=x+W;
-                pq.push(make_pair(x+W,i));
+        for(int i=1;i<=n;i++)
+        {
+            int w=graph[node][i];//node와 연결된 간선의 가중치
+            if(w==INF)continue;//연결되어 있지 않다면 넘어가기
+            if(cost+w<value[i])//만약 node를 거쳐 가는 경로가 다익스트라 값을 저장하는 value값보다 작다면 업데이트
+            {
+                value[i]=cost+w;
+                pq.push(make_pair(cost+w,i)); //값이 업데이트되었다면 우선순위 큐에 넣어서 나중에 검토
             }
         }
     }
-    cout<<Ddist[end]<<"\n";
-    
+    cout<<value[e]<<"\n";
     return 0;
 }
 
