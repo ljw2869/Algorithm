adf763345b5c1df4bc9a23b567165e08d1145bb9
ljw2869 <138024372+ljw2869@users.noreply.github.com>
2025-08-07 22:13:49 +0100

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b==c)
+        cout<<"correct!";
+    else
+        cout<<"wrong!";
+
+    return 0;
+}
+
+
